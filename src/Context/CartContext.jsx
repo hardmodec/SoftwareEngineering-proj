1020475b869270e29722ed8bd266f4b86195c117
jonggwanLee <jkkwan27@gmail.com>
2022-11-26 12:18:48 +0900

    help

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -8,7 +8,7 @@ const CartContext = createContext({
     menu1Cart1 : {dinnerid: 1, dinneramount: 1, menulist:[{menuid: 2, amount: 1}, {menuid: 1, amount: 1}]},
     menu2Cart1 : {dinnerid: 2, dinneramount: 1, menulist:[{menuid: 3, amount: 1}, {menuid: 2, amount: 1}, {menuid: 5, amount: 1}, {menuid: 1, amount: 1}]},
     menu3Cart1 : {dinnerid: 3, dinneramount: 1, menulist:[{menuid: 6, amount: 1}, {menuid: 7, amount: 1}, {menuid: 8, amount: 1}, {menuid: 1, amount: 1}]},
-    menu4Cart1 : [{dinnerid: 4, dinneramount: 1, menulist:[{menuid: 4, amount: 5}, {menuid: 9, amount: 1}, {menuid: 3, amount: 5}, {menuid: 2, amount: 1}, {menuid: 1, amount: 1}]}],
+    menu4Cart1 : {dinnerid: 4, dinneramount: 1, menulist:[{menuid: 4, amount: 5}, {menuid: 9, amount: 1}, {menuid: 3, amount: 5}, {menuid: 2, amount: 1}, {menuid: 1, amount: 1}]},
 
     AddMenu1 : () => {},
     AddMenu2 : () => {},
@@ -29,7 +29,7 @@ const CartProvider = ({children}) => {
     const [menu1Cart1, setMenu1Cart1] = useState({dinnerid: 1, dinneramount: 1, menulist:[{menuid: 2, amount: 1}, {menuid: 1, amount: 1}]})
     const [menu2Cart1, setMenu2Cart1] = useState({dinnerid: 2, dinneramount: 1, menulist:[{menuid: 3, amount: 1}, {menuid: 2, amount: 1}, {menuid: 5, amount: 1}, {menuid: 1, amount: 1}]})
     const [menu3Cart1, setMenu3Cart1] = useState({dinnerid: 3, dinneramount: 1, menulist:[{menuid: 6, amount: 1}, {menuid: 7, amount: 1}, {menuid: 8, amount: 1}, {menuid: 1, amount: 1}]})
-    const [menu4Cart1, setMenu4Cart1] = useState([{dinnerid: 4, dinneramount: 1, menulist:[{menuid: 4, amount: 5}, {menuid: 9, amount: 1}, {menuid: 3, amount: 5}, {menuid: 2, amount: 1}, {menuid: 1, amount: 1}]}])
+    const [menu4Cart1, setMenu4Cart1] = useState({dinnerid: 4, dinneramount: 1, menulist:[{menuid: 4, amount: 5}, {menuid: 9, amount: 1}, {menuid: 3, amount: 5}, {menuid: 2, amount: 1}, {menuid: 1, amount: 1}]})
     const [dinnerCart, setDinnerCart] = useState([
         // {dinnerid: 1, dinneramount:1, menulist:[{menuid: 2, amount: 1}, {menuid: 1, amount: 1}]},
         // {dinnerid: 2, dinneramount:1, menulist:[{menuid: 3, amount: 1}, {menuid: 2, amount: 1}, {menuid: 5, amount: 1}, {menuid: 1, amount: 1}]},
@@ -39,9 +39,9 @@ const CartProvider = ({children}) => {
 
     const AddMenu1 = (menuid, amount) => {
         for (let i = 0; i < 2; i++) {
-            if (menu1Cart1.menulist[i].menuid === menuid) {
+            if (menu1Cart1 && menu1Cart1.menulist[i].menuid === menuid) {
+                setMenu1Cart1(menu1Cart1.menulist[i].amount= amount)
             }
-        setMenu1Cart1(menu1Cart1.menulist.amount= amount)
         }
     }
     // const ResetMenu1 = (menuid, amount) => {
@@ -49,9 +49,9 @@ const CartProvider = ({children}) => {
     // }
     const AddMenu2 = (menuid, amount) => {
         for (let i = 0; i < 4; i++) {
-            if (menu2Cart1.menulist[i].menuid === menuid) {
+            if (menu2Cart1 && menu2Cart1.menulist[i].menuid === menuid) {
+                setMenu2Cart1(menu2Cart1.menulist[i].amount= amount)
             }
-        setMenu2Cart1(menu2Cart1.menulist.amount= amount)
         }
     }
     // const ResetMenu2 = (menuid, amount) => {
@@ -59,9 +59,9 @@ const CartProvider = ({children}) => {
     // }
     const AddMenu3 = (menuid, amount) => {
         for (let i = 0; i < 4; i++) {
-            if (menu3Cart1.menulist[i].menuid === menuid) {
+            if (menu3Cart1 && menu3Cart1.menulist[i].menuid === menuid) {
+                setMenu3Cart1(menu3Cart1.menulist[i].amount= amount)
             }
-        setMenu3Cart1(menu3Cart1.menulist.amount= amount)
         }
     }
     // const ResetMenu3 = (menuid, amount) => {
@@ -76,9 +76,9 @@ const CartProvider = ({children}) => {
     // }
     const AddMenu4 = (menuid, amount) => {
         for (let i = 0; i < 5; i++) {
-            if (menu4Cart1.menulist[i].menuid === menuid) {
+            if (menu4Cart1 && menu4Cart1.menulist[i].menuid === menuid) {
             }
-        setMenu4Cart1(menu4Cart1.menulist.amount= amount)
+        setMenu4Cart1(menu4Cart1.menulist[i].amount= amount)
         }
     }
     // const menuList = dinnerCart.map((menu, index) => (<div className="shoppinglist" key={index}>{menu.dinnerid}, {menu.menulist[0].menuid}<button className="btn1"></button></div>));
--- src/Customer/Menupage/Main.jsx
+++ src/Customer/Menupage/Main.jsx
@@ -1,313 +1,169 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Header from '../../Component/Header';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import ButtonBase from '@mui/material/ButtonBase';
-import Colortheme1 from '../../Component/Colortheme1';
-import { purple } from '@mui/material/colors';
-import ControlledOpenSelect from '../../Component/Select';
-import {dinner, menustyle, basicmenu} from "../../Component/item"
-import {useState} from 'react';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import NativeSelect from '@mui/material/NativeSelect';
-import CartContext, {CartProvider, CartConsumer} from '../../Context/CartContext'
-import Detailmenu from '../../Component/Detailbutton'
-import "./menu.css"
-import {useContext} from "react"
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+// import Container from '@mui/material/Container';
+// import Grid from '@mui/material/Grid';
+// import ImageList from '@mui/material/ImageList';
+// import ImageListItem from '@mui/material/ImageListItem';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import Header from '../../Component/Header';
+// import { styled } from '@mui/material/styles';
+// import Paper from '@mui/material/Paper';
+// import ButtonBase from '@mui/material/ButtonBase';
+// import Colortheme1 from '../../Component/Colortheme1';
+// import { purple } from '@mui/material/colors';
+// import ControlledOpenSelect from '../../Component/Select';
+// import {dinner, menustyle, basicmenu} from "../../Component/item"
+// import {useState} from 'react';
+// import InputLabel from '@mui/material/InputLabel';
+// import FormControl from '@mui/material/FormControl';
+// import NativeSelect from '@mui/material/NativeSelect';
+// import CartContext, {CartProvider, CartConsumer} from '../../Context/CartContext'
+// import Detailmenu from '../../Component/Detailbutton'
+// import "./menu.css"
+// import {useContext} from "react"
 
-export function CustomizedButtons() {
-  const Cart = React.useContext(CartContext)
-  const {menu1Cart1, menu2Cart1, menu3Cart1, menu4Cart1, dinnerCart,
-        AddMenu1, AddMenu2, AddMenu3, AddMenu4, AddDinner,} = Cart
-  // const [ordercart, setOrdercart] = useState([])
-  // setOrdercart([...ordercart, menucart])
-  return (
-    <ColorButton variant="contained" onClick={{}}>장바구니에 추가</ColorButton>
-  );
-}
+// export function CustomizedButtons() {
+//   const Cart = React.useContext(CartContext)
+//   const {menu1Cart1, menu2Cart1, menu3Cart1, menu4Cart1, dinnerCart,
+//         AddMenu1, AddMenu2, AddMenu3, AddMenu4, AddDinner,} = Cart
+  
+//   return (
+//     <ColorButton variant="contained" onClick={{}}>장바구니에 추가</ColorButton>
+//   );
+// }
 
-function Menupage() {
-  // const [ordercart, setOrdercart] = useContext()
-  const Cart = React.useContext(CartContext)
-  const {menu1Cart1, menu2Cart1, menu3Cart1, menu4Cart1, dinnerCart,
-        AddMenu1, AddMenu2, AddMenu3, AddMenu4, AddDinner1, AddDinner2, AddDinner3, AddDinner4} = Cart
-  return (
-    <Container>
-    <Container Fixed>
-    <Box sx={{ flexGrow: 1, margin: 10}} >
-      <Grid container rowSpacing={8} columnSpacing={9}>
-        <Grid item xs={6}>
-          <Item elevation={8} sx={{borderRadius:'25px'}}>
-          <div className="menu1">
-            <h3>발렌타인 디너(Valentine dinner)</h3>
-            <ul>
-              <li>와인 1잔 <Detailmenu dinnerid={1} menuid={2}/> </li>
-              <li>스테이크 <Detailmenu dinnerid={1} menuid={1}/> </li>
-            </ul>
+// function Menupage() {
+//   // const [ordercart, setOrdercart] = useContext()
+//   const Cart = React.useContext(CartContext)
+//   const {menu1Cart1, menu2Cart1, menu3Cart1, menu4Cart1, dinnerCart,
+//         AddMenu1, AddMenu2, AddMenu3, AddMenu4, AddDinner1, AddDinner2, AddDinner3, AddDinner4} = Cart
+//   return (
+//     <Container>
+//     <Container Fixed>
+//     <Box sx={{ flexGrow: 1, margin: 10}} >
+//       <Grid container rowSpacing={8} columnSpacing={9}>
+//         <Grid item xs={6}>
+//           <Item elevation={8} sx={{borderRadius:'25px'}}>
+//           <div className="menu1">
+//             <h3>발렌타인 디너(Valentine dinner)</h3>
+//             <ul>
+//               <li>와인 1잔 <Detailmenu dinnerid={1} menuid={2}/> </li>
+//               <li>스테이크 <Detailmenu dinnerid={1} menuid={1}/> </li>
+//             </ul>
             
 
-            <ControlledOpenSelect dinnerid={1}/> 
+//             <ControlledOpenSelect dinnerid={1}/> 
 
-            <h3>기본가격: ₩ {dinner[0].price} / ₩ {dinner[0].price+2000} / ₩ {dinner[0].price+5000}</h3>
+//             <h3>기본가격: ₩ {dinner[0].price} / ₩ {dinner[0].price+2000} / ₩ {dinner[0].price+5000}</h3>
             
-              <CustomizedButtons onClick={AddDinner1}>
-              </CustomizedButtons>  
-            </div>
-          </Item>
-        </Grid>
+//               <CustomizedButtons onClick={AddDinner1}>
+//               </CustomizedButtons>  
+//             </div>
+//           </Item>
+//         </Grid>
 
-        <Grid item xs={6}>
-          <Item elevation={8} sx={{borderRadius:'25px'}}>
-          <div className="menu1">
+//         <Grid item xs={6}>
+//           <Item elevation={8} sx={{borderRadius:'25px'}}>
+//           <div className="menu1">
 
-            <h3>프렌치 디너(French dinner)</h3>
-              <ul>
-                <li>커피 1잔 <Detailmenu dinnerid={2} menuid={3}/> </li>
-                <li>와인 1잔 <Detailmenu dinnerid={2} menuid={2}/> </li>
-                <li>샐러드   <Detailmenu dinnerid={2} menuid={5}/> </li>
-                <li>스테이크 <Detailmenu dinnerid={2} menuid={1}/> </li>
-              </ul>
-              <ControlledOpenSelect></ControlledOpenSelect>
+//             <h3>프렌치 디너(French dinner)</h3>
+//               <ul>
+//                 <li>커피 1잔 <Detailmenu dinnerid={2} menuid={3}/> </li>
+//                 <li>와인 1잔 <Detailmenu dinnerid={2} menuid={2}/> </li>
+//                 <li>샐러드   <Detailmenu dinnerid={2} menuid={5}/> </li>
+//                 <li>스테이크 <Detailmenu dinnerid={2} menuid={1}/> </li>
+//               </ul>
+//               <ControlledOpenSelect></ControlledOpenSelect>
 
-            <h3>기본가격: ₩ {dinner[1].price} / ₩ {dinner[1].price+2000} / ₩ {dinner[1].price+5000}</h3>
-          <CustomizedButtons onClick={AddDinner2}>
-          </CustomizedButtons> 
-          </div>
+//             <h3>기본가격: ₩ {dinner[1].price} / ₩ {dinner[1].price+2000} / ₩ {dinner[1].price+5000}</h3>
+//           <CustomizedButtons onClick={AddDinner2}>
+//           </CustomizedButtons> 
+//           </div>
 
-          </Item>
-        </Grid>
+//           </Item>
+//         </Grid>
 
-        <Grid item xs={6}>
-          <Item elevation={8} sx={{borderRadius:'25px'}}>
-          <div className="menu1">
+//         <Grid item xs={6}>
+//           <Item elevation={8} sx={{borderRadius:'25px'}}>
+//           <div className="menu1">
 
-            <h3>잉글리시 디너(English dinner)</h3>
-                <ul>
-                  <li>에그 스크램블 <Detailmenu dinnerid={3} menuid={6}/> </li>
-                  <li>베이컨       <Detailmenu  dinnerid={3} menuid={7}/> </li>
-                  <li>빵           <Detailmenu  dinnerid={3} menuid={8}/> </li>
-                  <li>스테이크     <Detailmenu dinnerid={3} menuid={1}/> </li>
-                </ul>
-                <ControlledOpenSelect></ControlledOpenSelect>
+//             <h3>잉글리시 디너(English dinner)</h3>
+//                 <ul>
+//                   <li>에그 스크램블 <Detailmenu dinnerid={3} menuid={6}/> </li>
+//                   <li>베이컨       <Detailmenu  dinnerid={3} menuid={7}/> </li>
+//                   <li>빵           <Detailmenu  dinnerid={3} menuid={8}/> </li>
+//                   <li>스테이크     <Detailmenu dinnerid={3} menuid={1}/> </li>
+//                 </ul>
+//                 <ControlledOpenSelect></ControlledOpenSelect>
 
-            <h3>기본가격: ₩ {dinner[2].price} / ₩ {dinner[2].price+2000} / ₩ {dinner[2].price+5000}</h3>
-            <CustomizedButtons onClick={AddDinner3}>
-            </CustomizedButtons> 
-            </div>
+//             <h3>기본가격: ₩ {dinner[2].price} / ₩ {dinner[2].price+2000} / ₩ {dinner[2].price+5000}</h3>
+//             <CustomizedButtons onClick={AddDinner3}>
+//             </CustomizedButtons> 
+//             </div>
 
-          </Item>
-        </Grid>
+//           </Item>
+//         </Grid>
 
-        <Grid item xs={6}>
-          <Item elevation={8} sx={{borderRadius:'25px'}}>
-          <div className="menu1">
+//         <Grid item xs={6}>
+//           <Item elevation={8} sx={{borderRadius:'25px'}}>
+//           <div className="menu1">
 
-            <h3>샴페인 축제 디너(Champagne Feast dinner)</h3>
-                <ul>
-                  <li>샴페인 1병      <Detailmenu dinnerid={4} menuid={4}/> </li>
-                  <li>바게트빵 4조각  <Detailmenu dinnerid={4} menuid={9}/></li>
-                  <li>커피 1포트     <Detailmenu dinnerid={4} menuid={3}/> </li>
-                  <li>와인 1잔       <Detailmenu dinnerid={4} menuid={2}/> </li>
-                  <li>스테이크       <Detailmenu dinnerid={4} menuid={1}/> </li>
-                </ul>
-                <ControlledOpenSelect></ControlledOpenSelect>
-            <h3>₩ {dinner[3].price} / ₩ {dinner[3].price+2000} / ₩ {dinner[3].price+5000}</h3>
-            <h3>* 2인 식사 기준 *</h3>
+//             <h3>샴페인 축제 디너(Champagne Feast dinner)</h3>
+//                 <ul>
+//                   <li>샴페인 1병      <Detailmenu dinnerid={4} menuid={4}/> </li>
+//                   <li>바게트빵 4조각  <Detailmenu dinnerid={4} menuid={9}/></li>
+//                   <li>커피 1포트     <Detailmenu dinnerid={4} menuid={3}/> </li>
+//                   <li>와인 1잔       <Detailmenu dinnerid={4} menuid={2}/> </li>
+//                   <li>스테이크       <Detailmenu dinnerid={4} menuid={1}/> </li>
+//                 </ul>
+//                 <ControlledOpenSelect></ControlledOpenSelect>
+//             <h3>₩ {dinner[3].price} / ₩ {dinner[3].price+2000} / ₩ {dinner[3].price+5000}</h3>
+//             <h3>* 2인 식사 기준 *</h3>
             
-            <CustomizedButtons onClick={AddDinner4}>
-            </CustomizedButtons> 
-            </div>
+//             <CustomizedButtons onClick={AddDinner4}>
+//             </CustomizedButtons> 
+//             </div>
 
-          </Item>
-        </Grid>
-      </Grid>
-    </Box>
-    {/* <Grid item xs={12} >
-          <Item>xs=8</Item>
-      </Grid> */}
-    </Container>
-    </Container>
-  );
-}
+//           </Item>
+//         </Grid>
+//       </Grid>
+//     </Box>
+    
+//     </Container>
+//     </Container>
+//   );
+// }
 
 
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#e8eaf6',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  height: 700,
-  width: 460,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#e8eaf6',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+//   height: 700,
+//   width: 460,
+// }));
 
 export default Menupage;
 
 
-const ColorButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.getContrastText(purple[500]),
-  backgroundColor: '#7e57c2',
-  '&:hover': {
-    backgroundColor: purple[700],
-  bottom: 0
-  },
-}));
-
-
-
-
-
-// const DUMMY_ITEM_LIST = [
-//   {
-//     id: 1,
-//     name: '영귤섬 아이스티',
-//     packingState: '포장불가',
-//     price: 13000,
-//     amount: 1,
-//     isChecked: true,
+// const ColorButton = styled(Button)(({ theme }) => ({
+//   color: theme.palette.getContrastText(purple[500]),
+//   backgroundColor: '#7e57c2',
+//   '&:hover': {
+//     backgroundColor: purple[700],
+//   bottom: 0
 //   },
-//   {
-//     id: 2,
-//     name: '러블리 티 박스',
-//     packingState: '포장가능',
-//     price: 20000,
-//     amount: 1,
-//     isChecked: true,
-//   },
-//   {
-//     id: 3,
-//     name: '그린티 랑드샤 세트',
-//     packingState: '포장불가',
-//     price: 36000,
-//     amount: 1,
-//     isChecked: true,
-//   },
-// ];
+// }));
 
-// <ul>
-// {itemList.map(item => {
-//   return (
-//     <CartItem key={item.id} item={item} onChangeProps={onChangeProps} />
-//   );
-// })}
-// </ul>
-
-// const amountInputHandler = event => {
-//   onChangeProps(item.id, 'amount', +event.target.value);
-// };
-
-// const amountIncreaseHandler = event => {
-//   event.preventDefault();
-//   onChangeProps(item.id, 'amount', item.amount + 1);
-// };
-
-// const amountDecreaseHandler = event => {
-//   event.preventDefault();
-//   onChangeProps(item.id, 'amount', item.amount - 1);
-// };
-
-// useEffect(() => {
-//   setIsBtnValid(item.amount > 1);
-// }, [item.amount]);
-
-// const onChangeProps = (id, key, value) => {
-//   setItemList(prevState => {
-//     return prevState.map(obj => {
-//       if (obj.id === id) {
-//         return { ...obj, [key]: value };
-//       } else {
-//         return { ...obj };
-//       }
-//     });
-//   });
-// };
-
-// const totalCheckboxHandler = value => {
-//   setItemList(prevState => {
-//     return prevState.map(obj => {
-//       return { ...obj, isChecked: value };
-//     });
-//   });
-//   setTotalCheckboxisChecked(value);
-// };
+function Menupage(){
+  return(
+    <div>help</div>
+  )
+}
 
 
 
-// import ItemPrice from './ItemPrice';
-// import './SideBar.scss';
-
-// const SideBar = props => {
-//   const { totalPrice } = props;
-
-//   const [deliveryCost, setDeliveryCost] = useState(0);
-
-//   useEffect(() => {
-//     setDeliveryCost(totalPrice < 30000 ? (totalPrice ? 3000 : 0) : 0);
-//   }, [totalPrice]);
-
-//   const PRICE_CATEGORY_LIST = [
-//     {
-//       id: 1,
-//       title: '상품 금액',
-//       price: `+${totalPrice.toLocaleString('en')}`,
-//     },
-//     {
-//       id: 2,
-//       title: '상품 할인',
-//       price: '-0',
-//     },
-//     {
-//       id: 3,
-//       title: '포장비',
-//       price: '+0',
-//     },
-//     {
-//       id: 4,
-//       title: '부가 쇼핑백',
-//       price: '+0',
-//     },
-//     {
-//       id: 5,
-//       title: '배송비',
-//       price: `+${deliveryCost.toLocaleString('en')}`,
-//     },
-//   ];
-
-//   return (
-//     <section className="sidebar">
-//       <div className="totalAmountOfPrice">
-//         <ul>
-//           {PRICE_CATEGORY_LIST.map(ele => (
-//             <ItemPrice key={ele.id} title={ele.title} price={ele.price} />
-//           ))}
-//         </ul>
-//         <div className="expectedPrice">
-//           <p>결제 예상 금액</p>
-//           <p>
-//             <span>{(totalPrice + deliveryCost).toLocaleString('en')}</span>
-//             <span>원</span>
-//           </p>
-//         </div>
-//         <button>{`${(totalPrice + deliveryCost).toLocaleString(
-//           'en'
-//         )}원 주문하기`}</button>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export SideBar;
-
-// export default BasicGrid
-
-
-// export newcart
